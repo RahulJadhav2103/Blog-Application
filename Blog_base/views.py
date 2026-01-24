8acd3c6c8412304bca88fe55d923af1d57b65609
Rahul Jadhav <rahulhjadhav22@gmail.com>
2026-01-24 23:50:04 +0530

    Final

--- Blog_base/views.py
+++ Blog_base/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from blogs.models import Blog, Category
-from assignments.models import About, SocialLink
 from .forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
