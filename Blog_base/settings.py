ff4b05a1b6eb2bb9fee336bc99823aaa808a5332
Rahul Jadhav <rahulhjadhav22@gmail.com>
2026-02-04 14:33:57 +0530

    Production

--- Blog_base/settings.py
+++ Blog_base/settings.py
@@ -102,11 +102,10 @@ WSGI_APPLICATION = 'Blog_base.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
