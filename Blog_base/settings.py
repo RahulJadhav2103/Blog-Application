29fb6566d3cae55b836a67a3e233f6427fd624c1
Rahul Jadhav <rahulhjadhav22@gmail.com>
2026-02-04 19:47:11 +0530

    Production

--- Blog_base/settings.py
+++ Blog_base/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
